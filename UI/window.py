9f3fff9d6bb82a1c7ef11f4d26cffad0e611aeed
Nguyen Duc Tri <113581831+sh1nata-piash1nlin@users.noreply.github.com>
2024-11-25 23:18:19 +0700

    Update window.py

--- UI/window.py
+++ UI/window.py
@@ -16,6 +16,8 @@ class UI:
         self.background = pygame.image.load('assets/background2.jpeg')
         self.background = pygame.transform.scale(self.background, (self.width, self.height))
 
+        #font
+        self.font_path = 'assets/PressStart2P-Regular.ttf'  # Path to the font file
     # def draw_snake(self, snake_block, snake_list):
     #     for x in snake_list:
     #         pygame.draw.rect(self.screen, self.green, [x[0], x[1], snake_block, snake_block])
@@ -23,8 +25,11 @@ class UI:
     # def draw_food(self, foodx, foody, snake_block):
     #     pygame.draw.rect(self.screen, self.white, [foodx, foody, snake_block, snake_block])
 
+    def get_font(self, size):
+        return pygame.font.Font(self.font_path, size)
+
     def display_message(self, message):
-        font_style = pygame.font.SysFont("bahnschrift", 25)
+        font_style = pygame.font.SysFont("Press Start 2P", 25)
         mesg = font_style.render(message, True, self.red)
         self.screen.blit(mesg, [self.width / 6, self.height / 3])
 
